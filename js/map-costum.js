a596edbe348a7538de0b8fb0b4bbdc3b64e6a844
Aymará <aymara@sapuru.com.ar>
2017-11-01 21:20:19 -0300

    Elimino todos los archivos que no usamos y arreglo mapa

--- js/map-costum.js
+++ js/map-costum.js
@@ -9,7 +9,7 @@
             zoom: 14,
 
             // The latitude and longitude to center the map (always required)
-            center: new google.maps.LatLng(42.6610638, 21.1986866), // New York
+            center: new google.maps.LatLng(-31.5289506,-68.527952), // New York
 
 
             mapTypeControl: true,
@@ -27,7 +27,7 @@
                 position: google.maps.ControlPosition.LEFT_TOP
             },
 
-            // How you would like to style the map. 
+            // How you would like to style the map.
             // This is where you would paste any style found on Snazzy Maps.
             styles: [{
                 "featureType": "poi",
@@ -164,19 +164,19 @@
             }]
         };
 
-        // Get the HTML DOM element that will contain your map 
+        // Get the HTML DOM element that will contain your map
         // We are using a div with id="map" seen below in the <body>
         var mapElement = document.getElementById('map');
 
         // Create the Google Map using our element and options defined above
         var map = new google.maps.Map(mapElement, mapOptions);
 
-        // Icon Map 
+        // Icon Map
         var image = 'img/map-pin.png';
 
         // Let's also add a marker while we're at it
         var marker = new google.maps.Marker({
-            position: new google.maps.LatLng(42.6610638, 21.1986866),
+            position: new google.maps.LatLng(-31.5289506, -68.527952),
             map: map,
             icon: image,
             title: 'OAK Theme'
